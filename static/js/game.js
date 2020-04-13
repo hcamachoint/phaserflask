2dfd06f4fd3980ee1ebf28818772080b085b5370
dunicode <dunicode@TUX.localdomain>
2020-04-13 10:50:02 -0400

    first commit

---
+++ static/js/game.js
@@ -0,0 +1,25 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: 'game',
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+}
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  //
+}
+
+function create() {
+  //
+}
+
+function update() {
+  //
+}
